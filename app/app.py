7313ee82e4db4f0712c8d7852c9c6301f26a8a0a
DinarGaniev <dinar0607s@gmail.com>
2022-12-21 22:41:40 +0300

    lab7

--- app/app.py
+++ app/app.py
@@ -1,38 +1,106 @@
-from flask import Flask, render_template, request, url_for, redirect
-import pymysql.cursors
-from config import MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER
+import os
+from flask import Flask, render_template, send_file, abort, send_from_directory, request
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import login_required
 
 app = Flask(__name__)
 application = app
 
-connection = pymysql.connect(host=MYSQL_HOST,
-                            user=MYSQL_USER,
-                            password=MYSQL_PASSWORD,
-                            database=MYSQL_DATABASE,
-                            charset='utf8mb4',
-                            cursorclass=pymysql.cursors.DictCursor)
+app.config.from_pyfile('config.py')
+
+
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+
+from auth import bp as auth_bp, init_login_manager
+app.register_blueprint(auth_bp)
+
+
+init_login_manager(app)
+
+from models import *
+
+
+class Cub:
+    def __init__(self, length, width, height):
+        self.length = length
+        self.width  = width
+        self.height = height
+
+    @property
+    def volume(self):
+        return self.length * self.width * self.height
+
+class Square:
+    def __init__(self, length, width):
+        self.length = length
+        self.width  = width
+
+    @property
+    def area(self):
+        return self.width * self.length
+
+@app.route('/show_room/<int:room_id>')
+# @login_required
+def show_room(room_id):
+
+    room_number = room_id
+    height = Room.query.filter_by(room_id=room_id).first().height
+    width = Room.query.filter_by(room_id=room_id).first().width
+    length = Room.query.filter_by(room_id=room_id).first().length
+    
+    volume_room = Cub(height, width, length).volume
+    area_room = Square(length, width).area
+
+    return render_template('show_room.html', volume_room=volume_room, area_room=area_room,room_number=room_number)
+
+@app.route('/show_building/<int:building_id>')
+# @login_required
+def show_building(building_id):
+    building_number = building_id
+
+    return render_template('show_building.html',building_number=building_number)
+
+
+
+@app.route('/edit_buildings/<int:building_id>')
+# @login_required
+def edit_buildings(building_id):
+    building = Building.query.get(building_id)
+    return render_template('edit_buildings.html', building=building)
+
+@app.route('/edit_room/<int:room_id>')
+# @login_required
+def edit_room(room_id):
+    room = Room.query.get(room_id)
+    return render_template('edit_room.html', room=room)
+
 
 @app.route('/')
 def index():
-    with connection.cursor() as cursor:
-        sql = 'SELECT * FROM user'
-        cursor.execute(sql)
-        names= cursor.fetchall()
-    return render_template('index.html', names=names)
+    rooms = Room.query.all()
+    placements = Placements.query.all()
+    return render_template('index.html', rooms=rooms, placements=placements)
+
+
+# @app.route('/')
+# def index():
+#     name_buildings=Building.query.filter_by(building_id=1).first().name
+#     number_of_faculties = 1
+
+#     return render_template('index.html', name_buildings=name_buildings)
 
 
 
-@app.route('/', methods=["GET", "POST"])
-def post_new():
-    form = request.form.get('name')
-    with connection.cursor() as cursor:
-        sql = """INSERT INTO user VALUES (%s)"""
-        cursor.execute(sql, (form))
-    connection.commit()
-
-    with connection.cursor() as cursor:
-        sql = 'SELECT * FROM user'
-        cursor.execute(sql)
-        names = cursor.fetchall()
-        
-    return render_template('index.html', names=names)
--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: std-mysql
--- Время создания: Ноя 04 2022 г., 14:04
+-- Время создания: Дек 21 2022 г., 18:58
 -- Версия сервера: 5.7.26-0ubuntu0.16.04.1
 -- Версия PHP: 8.0.15
 
@@ -18,27 +18,357 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `std_1692_prestige_test_db`
+-- База данных: `std_1689_prestige`
 --
 CREATE DATABASE IF NOT EXISTS `prestige_test_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `prestige_test_db`;
+
+
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `user`
+-- Структура таблицы `alembic_version`
 --
 
-CREATE TABLE `user` (
-  `name` varchar(200) NOT NULL
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `user`
+-- Дамп данных таблицы `alembic_version`
 --
 
-INSERT INTO `user` (`name`) VALUES
-('Nikita'),
-('Dinar');
+INSERT INTO `alembic_version` (`version_num`) VALUES
+('c72e5d0f5942');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `buildings`
+--
+
+CREATE TABLE `buildings` (
+  `building_id` int(11) NOT NULL,
+  `departament_id` int(11) DEFAULT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `buildings`
+--
+
+INSERT INTO `buildings` (`building_id`, `departament_id`, `name`) VALUES
+(1, 1, 'Корпус А'),
+(2, 1, 'Корпус А');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `departaments`
+--
+
+CREATE TABLE `departaments` (
+  `departament_id` int(11) NOT NULL,
+  `departament_type_id` int(11) DEFAULT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `departaments`
+--
+
+INSERT INTO `departaments` (`departament_id`, `departament_type_id`, `name`) VALUES
+(1, 1, 'Факультет Информационных технологий'),
+(2, 1, 'факультет машиностроения');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `departament_types`
+--
+
+CREATE TABLE `departament_types` (
+  `departament_type_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `departament_types`
+--
+
+INSERT INTO `departament_types` (`departament_type_id`, `name`) VALUES
+(1, '1 Тип департамента'),
+(2, '2 тип департамента');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `placements`
+--
+
+CREATE TABLE `placements` (
+  `placement_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `floor` int(11) NOT NULL,
+  `number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `placements`
+--
+
+INSERT INTO `placements` (`placement_id`, `name`, `floor`, `number`) VALUES
+(1, 'Расположение', 2, 204);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(100) NOT NULL,
+  `role_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `room_id` int(11) NOT NULL,
+  `building_id` int(11) DEFAULT NULL,
+  `room_type_id` int(11) DEFAULT NULL,
+  `room_height_type_id` int(11) DEFAULT NULL,
+  `placement_id` int(11) DEFAULT NULL,
+  `width` int(11) NOT NULL,
+  `length` int(11) NOT NULL,
+  `height` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `rooms`
+--
+
+INSERT INTO `rooms` (`room_id`, `building_id`, `room_type_id`, `room_height_type_id`, `placement_id`, `width`, `length`, `height`) VALUES
+(1, 1, 1, 1, 1, 4, 6, 3),
+(2, 1, 1, 1, 1, 13, 5, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `room_height_type`
+--
+
+CREATE TABLE `room_height_type` (
+  `room_height_type_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `room_height_type`
+--
+
+INSERT INTO `room_height_type` (`room_height_type_id`, `name`) VALUES
+(1, 'Высота типа помещения');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `room_type`
+--
+
+CREATE TABLE `room_type` (
+  `room_type_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `room_type`
+--
+
+INSERT INTO `room_type` (`room_type_id`, `name`) VALUES
+(1, 'Тип комнаты');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `password_hash` varchar(200) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `alembic_version`
+--
+ALTER TABLE `alembic_version`
+  ADD PRIMARY KEY (`version_num`);
+
+--
+-- Индексы таблицы `buildings`
+--
+ALTER TABLE `buildings`
+  ADD PRIMARY KEY (`building_id`),
+  ADD KEY `fk_buildings_departament_id_departaments` (`departament_id`);
+
+--
+-- Индексы таблицы `departaments`
+--
+ALTER TABLE `departaments`
+  ADD PRIMARY KEY (`departament_id`),
+  ADD KEY `fk_departaments_departament_type_id_departament_types` (`departament_type_id`);
+
+--
+-- Индексы таблицы `departament_types`
+--
+ALTER TABLE `departament_types`
+  ADD PRIMARY KEY (`departament_type_id`);
+
+--
+-- Индексы таблицы `placements`
+--
+ALTER TABLE `placements`
+  ADD PRIMARY KEY (`placement_id`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_id`),
+  ADD KEY `fk_rooms_building_id_buildings` (`building_id`),
+  ADD KEY `fk_rooms_placement_id_placements` (`placement_id`),
+  ADD KEY `fk_rooms_room_height_type_id_room_height_type` (`room_height_type_id`),
+  ADD KEY `fk_rooms_room_type_id_room_type` (`room_type_id`);
+
+--
+-- Индексы таблицы `room_height_type`
+--
+ALTER TABLE `room_height_type`
+  ADD PRIMARY KEY (`room_height_type_id`);
+
+--
+-- Индексы таблицы `room_type`
+--
+ALTER TABLE `room_type`
+  ADD PRIMARY KEY (`room_type_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_users_login` (`login`),
+  ADD KEY `fk_users_role_id_roles` (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `buildings`
+--
+ALTER TABLE `buildings`
+  MODIFY `building_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `departaments`
+--
+ALTER TABLE `departaments`
+  MODIFY `departament_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `departament_types`
+--
+ALTER TABLE `departament_types`
+  MODIFY `departament_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `placements`
+--
+ALTER TABLE `placements`
+  MODIFY `placement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `room_height_type`
+--
+ALTER TABLE `room_height_type`
+  MODIFY `room_height_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `room_type`
+--
+ALTER TABLE `room_type`
+  MODIFY `room_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `buildings`
+--
+ALTER TABLE `buildings`
+  ADD CONSTRAINT `fk_buildings_departament_id_departaments` FOREIGN KEY (`departament_id`) REFERENCES `departaments` (`departament_id`);
+
+--
+-- Ограничения внешнего ключа таблицы `departaments`
+--
+ALTER TABLE `departaments`
+  ADD CONSTRAINT `fk_departaments_departament_type_id_departament_types` FOREIGN KEY (`departament_type_id`) REFERENCES `departament_types` (`departament_type_id`);
+
+--
+-- Ограничения внешнего ключа таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD CONSTRAINT `fk_rooms_building_id_buildings` FOREIGN KEY (`building_id`) REFERENCES `buildings` (`building_id`),
+  ADD CONSTRAINT `fk_rooms_placement_id_placements` FOREIGN KEY (`placement_id`) REFERENCES `placements` (`placement_id`),
+  ADD CONSTRAINT `fk_rooms_room_height_type_id_room_height_type` FOREIGN KEY (`room_height_type_id`) REFERENCES `room_height_type` (`room_height_type_id`),
+  ADD CONSTRAINT `fk_rooms_room_type_id_room_type` FOREIGN KEY (`room_type_id`) REFERENCES `room_type` (`room_type_id`);
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_role_id_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
